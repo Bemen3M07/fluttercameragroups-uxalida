b0c41274b34f0f36510fc52abdcb126b969ac565
uxue <uxeslo24@bemen3.cat>
2026-02-02 19:23:15 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
+import 'ui/camera_screen.dart';
+import 'ui/gallery_screen.dart';
+import 'ui/audio_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'P3 Multimedia',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+
+  final screens = [
+    CameraScreen(),
+    GalleryScreen(),
+    AudioScreen()
+  ];
+
+  final titles = ['Càmera', 'Imatges', 'Reproductor'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(titles[index])),
+      body: screens[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value) => setState(() => index = value),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.camera_alt), label: 'Càmera'),
+          BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Imatges'),
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Àudio'),
+        ],
       ),
     );
   }
